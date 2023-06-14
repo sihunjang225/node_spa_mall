a689de71256f93f7534d3d3696c386d3f5c3e864
Jang Sihun <chocoj1123@naver.com>
2023-06-14 17:08:30 +0900

    First Commit All files

---
+++ routes/carts.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const Cart = require("../schemas/cart.js");
+const Goods = require("../schemas/goods.js");
+
+router.get("/carts", async (req, res) => {
+  const carts = await Cart.find({});
+  const goodsIds = carts.map((cart) => {
+    return cart.goodsId;
+  });
+
+  const goods = await Goods.find({ goodsId: goodsIds });
+
+  const results = carts.map((cart) => {
+    return {
+      quantity: cart.quantitiy,
+      goods: goods.find((item) => item.goodsId === cart.goodsId),
+    };
+  });
+
+  res.json({
+    carts: results,
+  });
+});
+
+module.exports = router;
